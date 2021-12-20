2b31ce29f7b391f8ab8d5259c782968db76fb74f
Gilad Barnea <cr-gbarn-herolo@allot.com>
2021-12-20 21:27:14 +0200

    test_store.test_models.TestEntry (doesn't pass yet)

--- test/test_store/test_models.py
+++ test/test_store/test_models.py
@@ -1,8 +1,11 @@
 from test import TEST_START_ARROW
 from test.testutils import default_work, assert_raises
 from timefred.log import log
+from timefred.note import Note
 from timefred.store import Day, Activity, Entry
+from timefred.tag import Tag
 from timefred.time import XArrow
+import pytest
 
 class TestActivity:
     def test_ongoing(self):
@@ -51,3 +54,22 @@ class TestActivity:
             with assert_raises(ValueError, f'Cannot stop {got_to_office_activity.shortrepr()} before start time (tried to stop at {yesterday!r})'):
                 got_to_office_activity.stop(yesterday)
     
+class TestEntry:
+    @pytest.mark.skip('unskip when test_space.test_space_subclasses.TestNote.test_sanity passes')
+    def test_init_with_all_fields(self):
+        entry = Entry(start="02:00:00",
+                      end="03:00:00",
+                      synced=True,
+                      notes=[{"02:30:00": "PR-5000"}],
+                      tags=["meeting"])
+        assert entry
+        assert entry.start == XArrow.from_absolute("02:00:00")
+        assert entry.end == XArrow.from_absolute("03:00:00")
+        assert entry.synced is True
+        assert entry.notes == [Note({"02:30:00": "PR-5000"})]
+        assert entry.notes[0] == Note({"02:30:00": "PR-5000"})
+        assert entry.notes[0].time == XArrow.from_absolute("02:30:00")
+        assert entry.notes[0].content == "PR-5000"
+        assert entry.tags == [Tag("meeting")]
+        assert next(iter(entry.tags)) == Tag("meeting")
+        assert isinstance(next(iter(entry.tags)), Tag)
