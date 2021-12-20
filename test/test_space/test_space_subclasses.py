7387c7f9ab50c41fc2e6dbc032757f1e94385f77
Gilad Barnea <cr-gbarn-herolo@allot.com>
2021-12-20 21:33:02 +0200

    test_space.test_space_subclasses.TestNote passes

--- test/test_space/test_space_subclasses.py
+++ test/test_space/test_space_subclasses.py
@@ -1,9 +1,7 @@
 from timefred.note import Note
 from timefred.time import XArrow
-import pytest
 
 class TestNote:
-    # @pytest.mark.skip('doesnt work')
     def test_sanity(self):
         note = Note({"17:00": "PR-6091"})
         assert isinstance(note.time, XArrow)
